c99f02cabf652a0f95cbe3c2e2674077740fbb2e
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-13 20:17:36 -0500

    Added CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var phonesApi = require('./routes/phones-api');
 var configs = require('./configs/database');
+var cors = require('cors');
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', index);
 app.use('/api', phonesApi);
